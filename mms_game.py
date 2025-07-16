ee191f21a6895939decc025f35a25a1dc468dc43
Stefanie Wedel <wedel.stefanie@protonmail.com>
2025-07-16 15:03:06 +0200

    Add random number generator function

--- mms_game.py
+++ mms_game.py
@@ -1,8 +1,28 @@
+import random
+
+
+def generate_random_number(min_val=100, max_val=200):
+    """
+    Generate a random number between 100 and 200.
+
+    Function uses random.randint to generate the number.
+    Number returned directly
+    """
+    random_number = random.randint(min_val, max_val)
+    print(f"[DEBUG] Generated number: {random_number}")
+
+    return random_number
+
+
+
 def start_game():
     print("Welcome to the M&Ms Guessing Game!")
-    print("There are 42 M&Ms in the jar.")  # Hardcoded number
+    random_number = generate_random_number()
     print("You guessed 50.")               # Hardcoded guess
     print("Sorry, that's too high!")       # Hardcoded feedback
 
+
 if __name__ == "__main__":
     start_game()
+
+
