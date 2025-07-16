74ffccf5d0beaef14155e98ba7a6e45d4c19019c
Stefanie Wedel <wedel.stefanie@protonmail.com>
2025-07-16 09:24:44 +0200

    Add Initial .py File With Base Function

---
+++ mms_game.py
@@ -0,0 +1,8 @@
+def start_game():
+    print("Welcome to the M&Ms Guessing Game!")
+    print("There are 42 M&Ms in the jar.")  # Hardcoded number
+    print("You guessed 50.")               # Hardcoded guess
+    print("Sorry, that's too high!")       # Hardcoded feedback
+
+if __name__ == "__main__":
+    start_game()
